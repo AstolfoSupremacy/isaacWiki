31b95ada6ab2ff4ee5071c2e05ed8e7281d9ac92
Andresito <andr.gonzalezg@duocuc.cl>
2023-04-01 17:12:22 -0300

    proyecto anadido a git

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 3000, /* Cambia la velocidad del carrusel (en milisegundos) */
+      pause: "hover" /* Detiene el carrusel al pasar el cursor por encima */
+    });
+  });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const navbarToggler = document.querySelector(".navbar-toggler");
+//     const navbarCollapse = document.querySelector(".navbar-collapse");
+//     navbarToggler.addEventListener("click", () => {
+//       navbarToggler.classList.toggle("active");
+//       navbarCollapse.classList.toggle("show");
+//     });
+//   });
+// function activarBotonColapsar() {
+//   var botonColapsar = document.querySelector('.navbar-toggler');
+//   var menu = document.querySelector('.navbar-collapse');
+
+//   botonColapsar.addEventListener('click', function() {
+//     menu.classList.toggle('show');
+//   });
+// }
+// document.addEventListener('DOMContentLoaded', function() {
+//   activarBotonColapsar();
+// });
+$(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 3000, /* Cambia la velocidad del carrusel (en milisegundos) */
+      pause: "hover" /* Detiene el carrusel al pasar el cursor por encima */
+    });
+  });
