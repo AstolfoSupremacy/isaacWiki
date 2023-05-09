5c63e21129e6daab5c18e63ce1b4b3ffacc11c65
Andresito <andr.gonzalezg@duocuc.cl>
2023-05-09 18:20:58 -0400

    cambios esteticos en la pagina, agregado de web services con ajax, borrado de codigo y basura cambio en el validador para el index 2

--- js/script.js
+++ js/script.js
@@ -5,25 +5,6 @@ $(document).ready(function(){
     });
   });
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     const navbarToggler = document.querySelector(".navbar-toggler");
-//     const navbarCollapse = document.querySelector(".navbar-collapse");
-//     navbarToggler.addEventListener("click", () => {
-//       navbarToggler.classList.toggle("active");
-//       navbarCollapse.classList.toggle("show");
-//     });
-//   });
-// function activarBotonColapsar() {
-//   var botonColapsar = document.querySelector('.navbar-toggler');
-//   var menu = document.querySelector('.navbar-collapse');
-
-//   botonColapsar.addEventListener('click', function() {
-//     menu.classList.toggle('show');
-//   });
-// }
-// document.addEventListener('DOMContentLoaded', function() {
-//   activarBotonColapsar();
-// });
 $(document).ready(function(){
     $('.carousel').carousel({
       interval: 3000, /* Cambia la velocidad del carrusel (en milisegundos) */
